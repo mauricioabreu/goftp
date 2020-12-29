7769d343041a300fb3cb1eff7907b4dc65b32d43
Maurício Antunes <mauricio.abreua@gmail.com>
2020-12-28 22:47:29 -0300

    Format LIST results

--- list.go
+++ list.go
@@ -4,7 +4,10 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/user"
 	"path/filepath"
+	"strconv"
+	"syscall"
 )
 
 func (c *Connection) list(args []string) {
@@ -52,8 +55,8 @@ func (c *Connection) list(args []string) {
 			c.writeout("450 Requested file action not taken.")
 			return
 		}
-		for _, file := range files {
-			if _, err := fmt.Fprint(dc, file, c.lineterminator()); err != nil {
+		for _, name := range formatList(files) {
+			if _, err := fmt.Fprint(dc, name, c.lineterminator()); err != nil {
 				log.Println(err)
 				c.writeout("426 Connection closed; transfer aborted.")
 				return
@@ -67,4 +70,59 @@ func (c *Connection) list(args []string) {
 		}
 	}
 	c.writeout("226 Closing data connection. Requested file action successful.")
+}
+
+// Format a list of files/directories
+// Output:
+// drwxr-xr-x  13 mauricio.abreua  staff   416 Dec 28 13:31 videos
+// -rw-r--r--   1 mauricio.abreua  staff   431 Dec 27 16:17 foo.txt
+func formatList(names []string) []string {
+	listing := make([]string, 0)
+	for _, name := range names {
+		file, err := os.Open(name)
+		if err != nil {
+			log.Printf("Skipping %s. Reason %s", name, err)
+			continue
+		}
+		fileinfo, err := file.Stat()
+		mode := fileinfo.Mode().String()
+		var nlinks int
+		var user, group string
+		stat, ok := fileinfo.Sys().(*syscall.Stat_t)
+		if !ok {
+			nlinks = 1 // for systems that don't support inode links
+			user = "owner"
+			group = "group"
+		} else {
+			nlinks = links(stat)
+			user = getUserFromUID(int(stat.Uid))
+			group = getGroupFromGID(int(stat.Gid))
+		}
+		modtime := fileinfo.ModTime().Format("Jan 2 15:04")
+		listing = append(listing, fmt.Sprintf(
+			"%s %3d %-8s %-8s %8d %s %s", mode, nlinks, user, group, fileinfo.Size(), modtime, name))
+	}
+	return listing
+}
+
+func links(stat *syscall.Stat_t) int {
+	return int(stat.Nlink)
+}
+
+func getUserFromUID(uid int) string {
+	suid := strconv.Itoa(int(uid))
+	user, err := user.LookupId(suid)
+	if err != nil {
+		return suid // raw uid if the system does not have this info
+	}
+	return user.Name
+}
+
+func getGroupFromGID(gid int) string {
+	sgid := strconv.Itoa(int(gid))
+	group, err := user.LookupGroupId(sgid)
+	if err != nil {
+		return sgid // raw uid if the system does not have this info
+	}
+	return group.Name
 }
